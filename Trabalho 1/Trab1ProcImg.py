32b6430d00d35800b203fdb50aa7a65652369ffd
Gabriel Garcia Lorencetti <40743828+gabrielgarcia7@users.noreply.github.com>
2021-04-28 10:55:20 -0300

    Add files via upload

--- Trabalho 1/Trab1ProcImg.py
+++ Trabalho 1/Trab1ProcImg.py
@@ -1,8 +1,8 @@
 import numpy as np
 import math
 import random
-import matplotlib.pyplot as plt
-import matplotlib.image as mpimg
+#import matplotlib.pyplot as plt
+#import matplotlib.image as mpimg
 
 def Fone (x, y):
     return (x * y + 2 * y)
@@ -19,11 +19,35 @@ def Ffour (x, y):
 #def Ffive (x, y):
 
 def NormalizeIMG (matrix, max_value):
-    norm = np.linalg.norm(matrix)
-    matrix = matrix/norm  # normalized matrix [0,1]
-    matrix = matrix * max_value # normalizes range [0, max_value]
+    #norm = np.linalg.norm(matrix)
+    #matrix = matrix/norm  # normalized matrix [0,1]
+    #matrix = matrix * max_value # normalizes range [0, max_value]
+    imax = np.max(matrix)
+    imin = np.min(matrix)
+
+    matrix_norm = (matrix-imin)/(imax-imin)
+    matrix_norm = (matrix_norm*max_value)
+
+    
+
+
+    return matrix_norm
+
+
+def BitwiseShift (matrix, bit_shift):
+    
+    #print ("Input matrix: \n", matrix)
+
+    matrix = np.right_shift(matrix, bit_shift)
+    #print ("Output matrix after right shifting: \n", matrix) 
+
+    matrix = np.left_shift(matrix, bit_shift)
+    #print ("Output matrix after left shifting: \n", matrix) 
+
     return matrix
   
+def RSEf(g, r):
+    return math.sqrt((np.sum(np.square(g - r)))/(len(g) - 2))
 
 ####### input reading ######
 _img_name = input().rstrip() # read starting image's name
@@ -43,6 +67,7 @@ if (_func_used == 1):
     for x in range (_img_size):
         for y in range (_img_size):
             img[x,y] = Fone(x,y)
+    #print(img)
 
 if (_func_used == 2):
     for x in range (_img_size):
@@ -75,35 +100,49 @@ if (_func_used == 5):
 
 img = NormalizeIMG (img, 65535) # Normalizing image
 
+#print(img)
+
 ####################
 
 ###### Sampling image #######
 step = int (_img_size / _reduced_img_size)
-
+#print("red aq \n")
+#print(_reduced_img_size)
+red = int(_reduced_img_size)
+#np.set_printoptions(threshold=np.inf)
 new_img = np.zeros( (_reduced_img_size, _reduced_img_size) )
-for i in range (_reduced_img_size): #downsampling img into new_img
-    for j in range (_reduced_img_size):
+for i in range (red): #downsampling img into new_img
+    for j in range (red):
         new_img[i, j] = img[i * step, j * step]
+        #print(new_img[i,j])
+#print(new_img)
+
 
 ############################
 
 ###### Quantizing ########
 new_img = NormalizeIMG(new_img, 255)
 new_img = new_img.astype(np.uint8)
-#need bitwise shift
+#print(new_img)
+new_img = BitwiseShift(new_img, 1)
+#print(new_img)
 
 ##########################
 
 #### Comparing with reference image ####
 
-RSE = 0.0
-ref_img = np.load(filename) # loading reference image
 
-for i in range (_reduced_img_size): # RSE sum loop
-    for j in range (_reduced_img_size):
-        RSE = RSE + ( (img[i,j] - ref_img[i,j]) ** 2 )
 
-RSE = math.sqrt(RSE) # RSE square root
-print("%f" % RSE)
+ref_img = np.load(_img_name) # loading reference image
+
+# RSE = 0.0
+# for i in range (_reduced_img_size): # RSE sum loop
+#     for j in range (_reduced_img_size):
+#         RSE = RSE + ( (new_img[i,j] - ref_img[i,j]) ** 2 )
+
+# RSE = math.sqrt(RSE) # RSE square root
+# print("%.4f" % RSE)
+
+print("%.4f" % RSEf(new_img, ref_img))
 
 #################################
