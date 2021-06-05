6512e939df35e8e5dffae36715b9b891d8a9e9aa
Caio Basso <caioadbasso@hotmail.com>
2021-06-05 17:02:31 -0300

    Update Trab4ProcIMG.py
    added method 1 skeleton

--- Trabalho 4/Trab4ProcIMG.py
+++ Trabalho 4/Trab4ProcIMG.py
@@ -21,12 +21,58 @@ import math
 
 ####### functions definitions ######
 
+# Function for method 1
+def Adaptive_denoising(gamma, img, filter_size, mode, row):
+
+    crop_img = img[ row[0]:row[2],row[1]:row[3] ] #crops image based on coordenates from input
+
+    pad_size = (filter_size - 1) // 2 # calculating padding size for each side of image
+    img = np.pad(img, (pad_size, pad_size), 'symmetric') # adds padding to image
+
+    new_img = img
+    imgSizeX,imgSizeY = img.shape
+
+    if (mode == "average"): # if mode is average
+        dispn = np.std(crop_img) # standard deviation from cropped image
+        if dispn == 0:
+            dispn = 1
+
+        for i in range (pad_size, imgSizeX - pad_size):
+            for j in range (pad_size, imgSizeY - pad_size):
+                displ = np.std(img[i-pad_size:i+pad_size][j-pad_size:j+pad_size]) # standard deviation from image area --fixthis
+                centerl = np.mean(img[i-pad_size:i+pad_size][j-pad_size:j+pad_size]) # average from image area --fixthis
+                if displ == 0:
+                    displ = dispn
+
+                new_img[i][j] = img[i][j] - gamma * (dispn/displ) * (img[i][j] - centerl) # that one formula pdf
+
+       
+    else: # if mode is robust
+        q75, q25 = np.percentile(crop_img, [75, 25]) # gets iqr from cropped image
+        dispn = q75 - q25
+        if dispn == 0:
+            dispn = 1
+
+        for i in range (pad_size, imgSizeX - pad_size):
+            for j in range (pad_size, imgSizeY - pad_size):
+
+                q75, q25 = np.percentile(img[i-pad_size:i+pad_size][j-pad_size:j+pad_size], [75, 25]) # gets iqr from image area --fixthis
+                displ = q75 - q25
+                if displ == 0:
+                    displ = dispn
+
+                centerl = np.median(img[i-pad_size:i+pad_size][j-pad_size:j+pad_size]) # median from image area --fixthis
+                new_img[i][j] = img[i][j] - gamma * (dispn/displ) * (img[i][j] - centerl) # that one formula pdf
+
+    return new_img
 
 # Function for method 2
 def Constrained_least_square(gamma, img, filter):
     p = np.array([[0, -1, 0], [-1, 4, -1], [0, -1, 0]]) # inverse of Laplacian filter
-    
     # adding padding
+    # --fixthis (maybe)
+    img = np.pad(img, (1,1)) 
+
     N,M = img.shape
     a = int((N-3)/2)
     b = int((M-2)/2)
@@ -77,7 +123,7 @@ _gamma = float(input()) # read parameter γ
 
 
 if _func_used == 1: # if using method 1 (Adaptive Denoising)
-    #_row = ***************
+    _row = list(map(int, input().split()))
     _filter_size = int(input())
     _mode = input().rstrip() # read denoising mode: "average" or "robust"
 
@@ -95,7 +141,7 @@ ref_img = imageio.imread(_img_ref_name)
 
 ###### restoring image ######
 if _func_used == 1:
-
+    img = Adaptive_denoising(_gamma, img, _filter_size, _mode, _row)
 
 elif _func_used == 2:
     filter = Gaussian_filter(_filter_size, _sigma)
