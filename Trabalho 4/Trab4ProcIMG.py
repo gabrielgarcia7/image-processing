9de610ba7c47b222ba0fbf124763d259c39a3628
Gabriel Garcia Lorencetti <40743828+gabrielgarcia7@users.noreply.github.com>
2021-06-08 20:28:57 -0300

    adding comments

--- Trabalho 4/Trab4ProcIMG.py
+++ Trabalho 4/Trab4ProcIMG.py
@@ -1,14 +1,14 @@
 # -----------------------------------------------------------
-# Assignment 4: recovery and fourier
+# Assignment 4:  image restoration
 # SCC0251 — Image Processing
 # 
-# Prof. Lizeth Andrea Castellanos Beltran **********
-# Aluna PAE: Leo Sampaio Ferraz Ribeiro
-# Monitor: Flavio Salles
+# SCC5830/0251 — Prof. Moacir Ponti
+# Teaching Assistant: Leo Sampaio Ferraz Ribeiro
+# Teaching Assistant: Flavio Salles
 #
 # 2021.1
 # 
-# Trabalho feito por:
+# Made by:
 # Caio Augusto Duarte Basso - NUSP 10801173
 # Gabriel Garcia Lorencetti - NUSP 10691891
 # -----------------------------------------------------------
@@ -16,7 +16,6 @@
 
 import numpy as np
 import imageio
-import scipy
 import math
 
 
@@ -25,25 +24,23 @@ import math
 # Function for method 1
 def Adaptive_denoising(gamma, img, filter_size, mode, row):
 
-    #new_img = np.zeros(img.shape)
-    #print(row)
     crop_img = img[ row[0]:row[1],row[2]:row[3] ] #crops image based on coordenates from input
 
     pad_size = (filter_size - 1) // 2 # calculating padding size for each side of image
     img = np.pad(img, (pad_size, pad_size), 'symmetric') # adds padding to image
 
-    #print(crop_img)
-
-    new_img = img
     imgSizeX,imgSizeY = img.shape
+    new_img = np.zeros((imgSizeX, imgSizeY), np.int32)
 
     if (mode == "average"): # if mode is average
+        
         dispn = np.std(crop_img) # standard deviation from cropped image
         if dispn == 0:
             dispn = 1
+
         for i in range (pad_size, imgSizeX - pad_size):
             for j in range (pad_size, imgSizeY - pad_size):
-                a = 0
+
                 displ = np.std(img[i-pad_size:i+pad_size, j-pad_size:j+pad_size]) # standard deviation from image area --fixthis
                 centerl = np.mean(img[i-pad_size:i+pad_size, j-pad_size:j+pad_size]) # average from image area --fixthis
                 
@@ -54,14 +51,17 @@ def Adaptive_denoising(gamma, img, filter_size, mode, row):
 
    
     else: # if mode is robust
+
         q75, q25 = np.percentile(crop_img, [75, 25]) # gets iqr from cropped image
         dispn = q75 - q25
         if dispn == 0:
             dispn = 1
+        
         for i in range (pad_size, imgSizeX - pad_size):
             for j in range (pad_size, imgSizeY - pad_size):
 
                 q75, q25 = np.percentile(img[i-pad_size:i+pad_size, j-pad_size:j+pad_size], [75, 25]) # gets iqr from image area --fixthis
+                
                 displ = q75 - q25
                 centerl = np.median(img[i-pad_size:i+pad_size, j-pad_size:j+pad_size]) # median from image area --fixthis
                 
@@ -77,11 +77,13 @@ def Adaptive_denoising(gamma, img, filter_size, mode, row):
     
     return new_img
 
+
 # Function for method 2
 def Constrained_least_square(gamma, img, filter):
+
     p = np.array([[0, -1, 0], [-1, 4, -1], [0, -1, 0]]) # inverse of Laplacian filter
+    
     # adding padding
-    # --fixthis (maybe)
     x,y = img.shape
     img = np.pad(img, (3,3), 'symmetric')
 
@@ -94,14 +96,6 @@ def Constrained_least_square(gamma, img, filter):
     a = int((N-n)/2)
     b = int((M-m+1)/2)
     filter = np.pad(filter, (a, b))
-
-
-    # # moving to fourrier's domain
-    # img = scipy.fftpack.rfft2(img)
-    # p = scipy.fftpack.rfft2(p)
-    # filter = scipy.fftpack.rfft2(filter)
-    # h_ = np.conj(filter)
-
 
     # moving to fourrier's domain
     img = np.fft.rfft2(img)
@@ -118,10 +112,13 @@ def Constrained_least_square(gamma, img, filter):
     # clip image between [0,255]
     F = np.clip(F, 0, 255) 
 
+    #removing padding
     F = F[6:x+6,6:y+6]
 
     return F
 
+
+# Gaussian filter
 def Gaussian_filter (k = 3, sigma = 1.0):
     arx = np.arange ((-k // 2 ) + 1.0 , ( k // 2 ) + 1.0 )
     x, y = np.meshgrid ( arx , arx )
@@ -133,7 +130,9 @@ def Gaussian_filter (k = 3, sigma = 1.0):
 def RSME(g, r):
 	x, y = g.shape
 	return math.sqrt((np.sum(np.square(g - r)))/(x*y))
+
 ####################################
+
 
 ####### input reading #######
 _img_ref_name = input().rstrip() # read reference image's name
